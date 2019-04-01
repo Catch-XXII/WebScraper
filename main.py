d1d4f1ab3a6c98b211d0537253cb63129012017c
Catch-XXII <cgultekink@gmail.com>
2019-04-01 17:38:05 +0300

    Update main.py
    __init__ method updated

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import shutil
+import time
 import os
 
 class WebScraper(object):
-    def __init__(self,website):
-        self.website = website
+    def __init__(self):
+        self.website = input("Enter a website: ")
+        time.sleep(2)
+        self.__str__()
+        time.sleep(2)
+        self.file_creator()
+        time.sleep(2)
+        print("{} file was created successfully".format(self.filename))
+        time.sleep(2)
+        self.file_handler(self.filename)
+        self.file_mover()
         
     def __str__(self):
         print("Connecting to {}".format(self.website))
 
-    def file_creator(self,filename):
-        self.filename = filename
+
+    def file_creator(self):
+        self.filename = input("Enter a filename: ")
         if os.access(self.filename, os.R_OK):
             with open(self.filename,"w+") as f:
                 return f
-    
+
     def file_handler(self,filename):
         html_data = urlopen(self.website)
         soup = BeautifulSoup(html_data.read(), "html.parser")
@@ -28,3 +39,6 @@ class WebScraper(object):
         desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
         current_path = os.getcwd() + "\\" + self.filename
         shutil.move(current_path, desktop)
+
+
+
